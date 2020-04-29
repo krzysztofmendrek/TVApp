23d08ea04f7970165c32068803fbf96571941f23
Krzysztof Mendrek <kmendrek94@gmail.com>
2020-04-29 12:42:38 +0200

    Added display shows info, added possiblity to change displayed shows, added validation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import { mapListToDOMElements } from "./DOMinteractions.js";
+import { mapListToDOMElements, createDOMElem } from "./DOMinteractions.js";
+import { getShowsByKey } from "./requests.js";
 
 class TVApp {
   constructor() {
@@ -10,6 +11,8 @@ class TVApp {
 
   initializeApp = () => {
     this.connectDOMelements();
+    this.setupListeners();
+    this.fetchAndDisplayShows();
   }
 
   connectDOMelements = () => {
@@ -18,9 +21,6 @@ class TVApp {
 
     this.viewElems = mapListToDOMElements(listOfIds, 'id');
     this.showNameButtons = mapListToDOMElements(listOfShowNames, 'data-show-name');
-
-    console.log(this.viewElems);
-    console.log(this.showNameButtons);
   }
 
   setupListeners = () => {
@@ -31,6 +31,48 @@ class TVApp {
 
   setCurrentNameFilter = name => {
     this.selectedName = event.target.dataset.showName;
+    this.fetchAndDisplayShows();
+  }
+
+  fetchAndDisplayShows = () => {
+    getShowsByKey(this.selectedName).then(shows => this.renderCards(shows));
+  }
+
+  renderCards = shows => {
+    this.viewElems.showsWrapper.innerHTML = '';
+
+    for (const { show } of shows) {
+      this.createShowCard(show);
+    }
+  }
+
+  createShowCard = show => {
+    const divCard = createDOMElem('div', 'card');
+    const divCardBody = createDOMElem('div', 'card-body');
+    const h5 = createDOMElem('h5', 'card-title', show.name);
+    const btn = createDOMElem('button', 'btn btn-primary', 'Show details');
+    let img, p;
+
+    if(show.image) {
+      img = createDOMElem('img', 'card-img-top', null, show.image.medium);
+    } else {
+      img = createDOMElem('img', 'card-img-top', null, 'https://via.placeholder.com/210x295');
+    }
+
+    if(show.summary) {
+      p = createDOMElem('p', 'card-text', `${show.summary.replace(/(<([^>]+)>)/ig,"").slice(0, 100)}...`);
+    } else {
+      p = createDOMElem('p', 'card-text', "This show has no summary yet");
+    }
+    
+    divCard.appendChild(divCardBody);
+    divCardBody.appendChild(img);
+    divCardBody.appendChild(h5);
+    divCardBody.appendChild(p);
+    divCardBody.appendChild(btn);
+
+    this.viewElems.showsWrapper.appendChild(divCard);
+    
   }
 }
 
